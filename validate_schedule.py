461d18026de56e87efbcc6cdcb1b7711e7db1cca
Josh Gao <xvd9ca@virginia.edu>
2024-04-20 15:19:14 -0400

    minor updates

--- validate_schedule.py
+++ validate_schedule.py
@@ -1,1 +1,6 @@
 #Given a list of courses_taken and courses_wishlist under a Major, find whether or not there exists any conflicts in the schedule (e. g. missing requisites, time conficts, etc. )
+
+from helpers import Course, Major
+
+bacs = Major(name = "Computer Science, B.A.")
+
