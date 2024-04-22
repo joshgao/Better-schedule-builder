31bb5f9e6729808e5094e96a248f9e07a58fea90
Josh Gao <xvd9ca@virginia.edu>
2024-04-22 15:50:59 -0400

    added some comments on the example

--- exp.py
+++ exp.py
@@ -2,11 +2,11 @@ import json
 import requests
 
 #Example from CS3240 website
-clist = [('MATH','3100'), ('PSYC','2150'), ('STAT','2120')]
+clist = [('CS', '3100')] #these are the courses we're interested in
 url = 'https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?institution=UVA01&term=1248&page=1'
 
-for c in clist:
-    r = requests.get(url + '&subject=' + c[0] + '&catalog_nbr=' + c[1])
+for c in clist: #for each course
+    r = requests.get(url + '&subject=' + c[0] + '&catalog_nbr=' + c[1]) #get a json with the prefix and number
     for c in r.json():
-        print(c['subject'], c['catalog_nbr'] + '-' + c['class_section'], c['component'], c['descr'], \
-                  c['class_nbr'], c['class_capacity'], c['enrollment_available'])
+        print(c['subject'], c['catalog_nbr'] + '-' + c['class_section'], c['component'], c['descr'], 
+                  c['class_nbr'], c['class_capacity'], c['enrollment_available']) #c['meetings']
