cb55adb979662cee695ca1b440793e6ea80d56c7
Josh Gao <xvd9ca@virginia.edu>
2024-06-09 19:28:07 -0400

    brief changes to tester file

--- exp.py
+++ exp.py
@@ -1,11 +1,15 @@
 import json
 import requests
+import pandas as pd
 
 #Example from CS3240 website
 clist = [('CS', '3100')] #these are the courses we're interested in
 url = 'https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?institution=UVA01&term=1248&page=1'
+df = pd.DataFrame()
 
 for c in clist: #for each course
+    #TODO: Pull from the json, populaitng the dataframe with data below.
+
     r = requests.get(url + '&subject=' + c[0] + '&catalog_nbr=' + c[1]) #get a json with the prefix and number
     for c in r.json():
         print(c['subject'], c['catalog_nbr'] + '-' + c['class_section'], c['component'], c['descr'], 
