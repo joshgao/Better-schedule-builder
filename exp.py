d3db2ccb99da99668e08a86912bc05f964d40d98
Josh Gao <xvd9ca@virginia.edu>
2024-06-16 16:49:40 -0400

    setting up pandas with the course json environment

--- exp.py
+++ exp.py
@@ -5,12 +5,16 @@ import pandas as pd
 #Example from CS3240 website
 clist = [('CS', '3100')] #these are the courses we're interested in
 url = 'https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?institution=UVA01&term=1248&page=1'
-df = pd.DataFrame()
+df = pd.DataFrame(columns=['a','b','c','d', 'e', 'f', 'g', 'h'])
+
 
 for c in clist: #for each course
     #TODO: Pull from the json, populaitng the dataframe with data below.
 
     r = requests.get(url + '&subject=' + c[0] + '&catalog_nbr=' + c[1]) #get a json with the prefix and number
     for c in r.json():
+        df.loc[len(df)] = [c['subject'], c['catalog_nbr'],c['class_section'], c['component'], c['descr'], c['class_nbr'], c['class_capacity'], c['enrollment_available'] ]
         print(c['subject'], c['catalog_nbr'] + '-' + c['class_section'], c['component'], c['descr'], 
                   c['class_nbr'], c['class_capacity'], c['enrollment_available']) #c['meetings']
+        
+print(df)
